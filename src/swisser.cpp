6e6303f170347c98d9d445162221fa6ff619ea7c
Piero Toffanin <pt@masseranolabs.com>
2025-08-06 18:20:08 -0400

    PoC

---
+++ src/swisser.cpp
@@ -0,0 +1,246 @@
+/**
+Swisser
+Copyright (C) 2025 Piero Toffanin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <iostream>
+#include <vector>
+#include <tournament/tournament.h>
+#include <swisssystems/dutch.h>
+#include <swisssystems/common.h>
+
+#include "httplib.h"
+#include "json.hpp"
+using json = nlohmann::json;
+#define APP_VERSION "0.9.2"
+
+int main(int argc, char **argv) {
+    httplib::Server svr;
+    std::string host = "0.0.0.0";
+    int port = 8080;
+    bool verbose = false;
+
+    for (int i = 1; i < argc; i++) {
+        std::string param = std::string(argv[i]);
+
+        if ((param == "--host" || param == "-h") && i + 1 < argc) {
+            host = argv[i + 1];
+            i++; 
+        } else if ((param == "--port" || param == "-p") && i + 1 < argc) {
+            port = std::stoi(argv[i + 1]);
+            i++;
+        } else if (param == "--verbose" || param == "-v"){
+            verbose = true;
+            std::cout << "Verbose mode: on" << std::endl;
+        }
+    }
+
+    svr.Get("/", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Swisser v" APP_VERSION " is running", "text/plain");
+    });
+    svr.Get("/ping", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content(json( {{"swisser", "running"}, {"version", APP_VERSION }}).dump(), "application/json");
+    });
+    
+    svr.Post("/round", [&verbose](const httplib::Request &req, httplib::Response &res) {
+        std::string data = req.get_param_value("data");
+
+        if (verbose){
+            std::cout << "POST /round " << data << std::endl;
+        }
+        
+        try{
+            json j = json::parse(data);
+            json games = json::array();
+            if (j.contains("games")){
+                games = j.at("games");
+            }
+
+            tournament::Tournament tournament;
+            tournament.expectedRounds = j.at("rounds").get<int>();
+            tournament.playedRounds = games.size();
+            tournament.initialColor = tournament::COLOR_WHITE;
+
+            std::unordered_map<std::string, tournament::Player> players;
+            std::unordered_map<tournament::player_index, std::string> playerNames;
+            
+            tournament::player_index id = 1;
+
+            for (const auto &p : j.at("players")){
+                std::string name = p.at("name").get<std::string>();
+                tournament::rating rating = p.at("elo").get<int>();
+                
+                tournament::Player player(id, 0.0, rating);
+                playerNames[id] = name;
+                id++;
+
+                players[name] = player;
+            }
+
+            // int nextRound = games.size() + 1;
+
+            // Replay game history (optional)
+            int roundHist = 1;
+            for (const auto &results : games){
+                for (const auto &r: results){
+                    std::string white = r.at("white").get<std::string>();
+                    
+                    std::string black = "";
+                    if (r.contains("black")) black = r["black"].get<std::string>();
+                    
+                    bool bye = r.contains("bye") && r["bye"].get<bool>();
+                    
+                    float result = 0.0f;
+                    if (r.contains("result")) result = r["result"].get<float>();
+                    
+                    auto w = &players[white];
+
+                    // White played white
+                    // if (!bye) w->addColor(CPPDubovSystem::Color::WHITE);
+
+                    // Reset prev upfloat
+                    // w->setUpfloatPrevStatus(false);
+                    auto wm = &w->matches;
+
+                    if (!black.empty() && !bye){
+                        auto b = &players[black];
+
+                        auto bm = &b->matches;
+
+                        // // Black played black
+                        // b->addColor(CPPDubovSystem::Color::BLACK);
+
+                        // // Reset prev upfloat
+                        // b->setUpfloatPrevStatus(false);
+
+                        // // They played each other
+                        // if (!w->hasPlayedOpp(*b)){
+                        //     w->addOpp(b->getID());
+                        //     w->addOppRating(b->getRating());
+                        // }
+                        
+                        // if (!b->hasPlayedOpp(*w)){
+                        //     b->addOpp(w->getID());
+                        //     b->addOppRating(w->getRating());
+                        // }
+                        
+                        // // Keep track of upfloaters
+                        // if (w->getPoints() > b->getPoints()){
+                        //     b->incrementUpfloat();
+                        //     if (roundHist == nextRound - 1) b->setUpfloatPrevStatus(true);
+                        // }else if (w->getPoints() < b->getPoints()){
+                        //     w->incrementUpfloat();
+                        //     if (roundHist == nextRound - 1) w->setUpfloatPrevStatus(true);
+                        // }
+
+                        tournament::MatchScore ws;
+                        tournament::MatchScore bs;
+
+                        // White won
+                        if (result == 1.0f){
+                            ws = tournament::MATCH_SCORE_WIN;
+                            bs = tournament::MATCH_SCORE_LOSS;
+                            w->scoreWithoutAcceleration += tournament.pointsForWin;
+                            b->scoreWithoutAcceleration += tournament.pointsForLoss;
+                            
+                        // Draw
+                        }else if (result == 0.5f){
+                            ws = tournament::MATCH_SCORE_DRAW;
+                            bs = tournament::MATCH_SCORE_DRAW;
+                            w->scoreWithoutAcceleration += tournament.pointsForDraw;
+                            b->scoreWithoutAcceleration += tournament.pointsForDraw;
+                            
+                        // Black won
+                        }else if (result == 0.0f){
+                            ws = tournament::MATCH_SCORE_LOSS;
+                            bs = tournament::MATCH_SCORE_WIN;
+                            w->scoreWithoutAcceleration += tournament.pointsForLoss;
+                            b->scoreWithoutAcceleration += tournament.pointsForWin;
+                        }
+
+                        wm->emplace_back(b->id,
+                            tournament::COLOR_WHITE,
+                            ws,
+                            true,
+                            true);
+                        bm->emplace_back(w->id,
+                            tournament::COLOR_BLACK,
+                            bs,
+                            true,
+                            true);
+                    }else if (bye){
+                        wm->emplace_back(w->id,
+                            tournament::COLOR_NONE,
+                            tournament::MATCH_SCORE_WIN,
+                            false,
+                            true);
+                    }
+                }
+
+                roundHist++;
+            }
+            
+            for (auto &p : players){
+                tournament.playersByRank.push_back(p.second.id);
+                tournament.players.push_back(std::move(p.second));
+            }
+            
+            const swisssystems::SwissSystem swissSystem = swisssystems::DUTCH;
+
+            // std::vector<CPPDubovSystem::Match> pairings = tournament.generatePairings(nextRound);
+            const swisssystems::Info &info = swisssystems::getInfo(swissSystem);
+            std::list<swisssystems::Pairing> roundPairs;
+            json pairs = json::array();
+
+            roundPairs = info.computeMatching(std::move(tournament), nullptr);
+            swisssystems::sortResults(roundPairs, tournament);
+
+            for (const swisssystems::Pairing &p : roundPairs){
+                json m = json::object();
+                m["white"] = playerNames[p.white];
+
+                if (p.white == p.black){
+                    m["bye"] = true;
+                }else{
+                    m["black"] = playerNames[p.black];
+                }
+
+                pairs.push_back(m);
+            }
+
+            std::string out = pairs.dump();
+            if (verbose){
+                std::cout << "--> " << out << std::endl;
+            }
+            
+            res.set_content(out, "application/json");
+        }catch (const std::exception& e) {
+            json j;
+            j["error"] = e.what();
+            std::string out = j.dump();
+            if (verbose){
+                std::cout << "--> " << out << std::endl;
+            }
+
+            res.status = httplib::StatusCode::BadRequest_400;
+            res.set_content(out, "application/json");
+        }
+    });
+
+    std::cout << "Running swisser v" APP_VERSION " on " << host << ":" << port << std::endl;
+    svr.listen(host, port);
+    
+    return 0;
+}
